447e59934cde18370a7b8013bdc363c81728da69
Nursultan <nursultan_kas@icloud.com>
2024-06-20 10:15:46 -0400

    KAN-19 1)Fix bugs with hiding password 2) Changed vpn icon  to eye

--- src/app/main/register/tabs/FirebaseRegisterTab.js
+++ src/app/main/register/tabs/FirebaseRegisterTab.js
@@ -13,6 +13,7 @@ import * as yup from 'yup';
 /**
  * Form Validation Schema
  */
+
 const schema = yup.object().shape({
   displayName: yup.string().required('You must enter display name'),
   email: yup.string().email('You must enter a valid email').required('You must enter an email address'),
