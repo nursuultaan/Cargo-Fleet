8b25dc7a42a9d14b3d2168fb92e371babdfdd007
Nursultan <nursultan_kas@icloud.com>
2024-06-21 13:14:12 -0400

    Updated weather widget to display current weather based on IP-identified location.

--- src/app/main/apps/dashboard/store/widgetsSlice.js
+++ src/app/main/apps/dashboard/store/widgetsSlice.js
@@ -1,36 +1,13 @@
 import { createEntityAdapter, createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { changeWeatherWidgetData,getWeather } from './weatherWidgetHelpers';
 
-async function getIP() {
-  const res = await axios.get('https://api.ipify.org');
-  return res.data;
-}
-
-async function getLocationByIP() {
-  const ip = await getIP();
-  const locationData = await axios.get(`https://ipinfo.io/${ip}?token=1ef1055fb92fa6`);
-  return locationData.data;
-}
-
-async function getCityWeather() {
-  const ip = await getIP();
-  const location = await getLocationByIP();
-  console.log(location);
-  const res = await axios.get(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location.city.capitalize()}?unitGroup=metric&key=XGBEDGJ87QKB94WDUSDZ7MCBJ&contentType=json`
-  );
-  console.log("Weather :",res);
-  const weatherData = await res.data;
-  return "Weather :";
-}
 
 export const getWidgets = createAsyncThunk('projectDashboardApp/widgets/getWidgets', async () => {
   const response = await axios.get('/api/project-dashboard-app/widgets');
-  const data = await response.data;
-
-  console.log(await getCityWeather());
-
-  return data;
+  const widgetData = await response.data;
+  const weather = await getWeather();
+  return changeWeatherWidgetData(weather, widgetData);
 });
 
 const widgetsAdapter = createEntityAdapter({});
