382f57a00869561f927f374a658aef523553a421
amantur <you@example.com>
2024-06-21 03:15:42 +0200

    Changes Title and Description

--- src/app/main/login/Login.js
+++ src/app/main/login/Login.js
@@ -80,13 +80,13 @@ function Login() {
             <motion.div initial={{ opacity: 0, y: 40 }} animate={{ opacity: 1, y: 0, transition: { delay: 0.2 } }}>
               <Typography variant="h3" color="inherit" className="font-semibold leading-tight">
                 Welcome <br />
-                to the <br /> FUSE React!
+                to the <br /> Cargo Fleet!
               </Typography>
             </motion.div>
 
             <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 0.3 } }}>
               <Typography variant="subtitle1" color="inherit" className="mt-32">
-                Powerful and professional admin template for Web Applications, CRM, CMS, Admin Panels and more.
+                Vehicle management tool that can allow a tracking company to manage its truck fleet
               </Typography>
             </motion.div>
           </div>
