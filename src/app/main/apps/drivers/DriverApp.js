bfc79b8533e89cb3f8798a0480ecb0f0c43b99b0
Nursultan <nursultan_kas@icloud.com>
2024-07-15 13:14:09 -0400

    Fix header issues

--- src/app/main/apps/drivers/DriverApp.js
+++ src/app/main/apps/drivers/DriverApp.js
@@ -4,9 +4,9 @@ import DriverHeader from './DriverHeader';
 
 const DriverApp = () => {
   return (
-    <section className={'py-20'}>
+    <section className="py-20">
       <DriverHeader />
-      <div className={'p-20'}>
+      <div className="p-20">
         <DriversTable />
       </div>
     </section>
