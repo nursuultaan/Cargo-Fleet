660ba021d58a868c6a488e852d95bf313484f45d
Kazbek Abraliev <kazbek.abraliev@gmail.com>
2024-08-07 22:56:06 -0400

    Vehicle API endpoints tests with Cypress are added

--- cypress/e2e/API_Testing/vehicles_api.cy.js
+++ cypress/e2e/API_Testing/vehicles_api.cy.js
@@ -1,10 +1,8 @@
-const { it } = require('date-fns/locale');
-
 describe('Vehicles HTTP Requests', () => {
   const TOKEN = 'Zb84MzAROCrhmF6t';
   const VEHICLES_URL = 'https://cargofleet-api.fly.dev/team1/api/vehicles';
   // GET all vehicles endpoint
-  it('GET All Vehicles', () => {
+  it('GET Vehicles', () => {
     cy.request({
       method: 'GET',
       url: `${VEHICLES_URL}`,
@@ -17,7 +15,7 @@ describe('Vehicles HTTP Requests', () => {
   });
 
   // GET vehicle by ID
-  it('GET All Vehicles', () => {
+  it('GET Vehicle by ID', () => {
     cy.request({
       method: 'GET',
       url: `${VEHICLES_URL}/99`,
@@ -29,33 +27,84 @@ describe('Vehicles HTTP Requests', () => {
       .should('equal', 200);
   });
 
+  // testID will be saved when create vehicle, used when update and delete requests
+  let testId;
+
   // Create vehicle with POST method
-  it('Create vehicle', () => {
+  it('POST - Create Vehicle', () => {
+    // Hardcoded data + dynamic data for model:
+    // const createVehicleData = {
+    //   // model: 'CF24 Created by CypressAPI',
+    //   model: Math.random().toString(5).substring(2),
+    //   manufacture_year: '2004-09-22',
+    //   image_url: 'https://loremflickr.com/300/300/truck',
+    //   plate_number: 'TAYBADVZ',
+    //   engine_number: 'JZ4M0ZL6N3JRBEE4',
+    //   fuel_type: 'gasoline',
+    //   active: true,
+    //   issues: [{}]
+    // };
+    cy.fixture('createVehicle').then(fixtureData => {
+      const createVehicleData = fixtureData;
+      cy.request({
+        method: 'POST',
+        url: `${VEHICLES_URL}`,
+        headers: {
+          Authorization: TOKEN,
+          ContentType: 'application/json'
+        },
+        body: createVehicleData
+      })
+        // .its('status')
+        // .should('equal', 201)
+        .then(response => {
+          expect(response.status).to.eq(201);
+          expect(response.body.model).to.eq(createVehicleData.model);
+          // testID saved and will be used for update and delete requests
+          testId = response.body.id;
+        });
+    });
+  });
+
+  // Update vehicle info with PUT method
+  it('PUT - Update Vehicle', () => {
+    console.log('testId to update', testId);
+    cy.fixture('updateVehicle').then(fixtureData => {
+      const updateVehicleData = fixtureData;
+
+      cy.request({
+        method: 'PUT',
+        url: `${VEHICLES_URL}/${testId}`,
+        headers: {
+          Authorization: TOKEN,
+          ContentType: 'application/json'
+        },
+        body: updateVehicleData
+      })
+        // .its('status')
+        // .should('equal', 200);
+        .then(response => {
+          expect(response.status).to.eq(200);
+          expect(response.body.model).to.eq(updateVehicleData.model);
+        });
+    });
+  });
+
+  // Remove vehicle using ID
+  it('DELETE Vehicle', () => {
+    console.log('testId to delete', testId);
     cy.request({
-      method: 'POST',
-      url: `${VEHICLES_URL}`,
+      method: 'DELETE',
+      url: `${VEHICLES_URL}/${testId}`,
       headers: {
         Authorization: TOKEN,
         ContentType: 'application/json'
-      },
-      body: {
-        model: 'CargoFleet24 CypressAPI',
-        manufacture_year: '2004-09-22',
-        image_url: 'https://loremflickr.com/300/300/truck',
-        plate_number: 'TAYBADVZ',
-        engine_number: 'JZ4M0ZL6N3JRBEE4',
-        fuel_type: 'gasoline',
-        created_at: '2023-12-29T03:40:52.975Z',
-        updated_at: '2023-12-29T03:40:52.975Z',
-        active: true,
-        issues: [{}]
       }
     })
-      .its('status')
-      .should('equal', 201);
+      // .its('status')
+      // .should('equal', 204);
+      .then(response => {
+        expect(response.status).to.eq(204);
+      });
   });
-
-  // Update vehicle info
-
-  // Remove vehicle using ID
 });
